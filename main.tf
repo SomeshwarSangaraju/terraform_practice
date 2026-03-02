68fd8d817fec3e9bb2880da8e449036690c257a2
SSR <someshwar.sangaraju19@gmail.com>
2026-03-02 19:32:15 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "terraform" {
   ami = data.aws_ami.ami_id.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags={
     Name="Terraform"
